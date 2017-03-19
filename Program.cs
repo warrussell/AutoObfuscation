81c4f299a2558445b5cf7bf2148905fa707950fb
Warren <warrussell@gmail.com>
2017-03-18 17:21:34 -0700

    New SecureFloat class. Program modifies Unity projects

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace AutoObfuscation
 {
@@ -18,30 +22,86 @@ namespace AutoObfuscation
             Console.ReadLine();
             var workspace = MSBuildWorkspace.Create();
 
-            var oldSolution = workspace.OpenSolutionAsync(path).Result;
-            Solution newSolution = oldSolution;
-            foreach (var projectId in newSolution.ProjectIds)
+            var solution = workspace.OpenSolutionAsync(path).Result;
+            foreach (var projectId in solution.ProjectIds)
             {
-                var project = newSolution.GetProject(projectId);
+                Console.WriteLine("Found Project: " + projectId);
+                var project = solution.GetProject(projectId);
                 foreach (var documentId in project.DocumentIds)
                 {
                     var document = project.GetDocument(documentId);
-                    
-                    var newDocument = Obfuscate(document);
-
-                    newSolution = newDocument.Project.Solution;
+                    document = Obfuscate(document);
+                    project = document.Project;
                 }
+
+                solution = project.Solution;
             }
 
-            workspace.TryApplyChanges(newSolution);
+            Console.WriteLine(workspace.TryApplyChanges(solution));
+            Console.Read();
+
+            /* Replace Method Return Type
+            syncInterface
+              .ReplaceNodes(
+                  syncInterface.Members.OfType<MethodDeclarationSyntax>(),
+                  (a, b) =>
+                      b.WithReturnType(
+                          SyntaxFactory.GenericName("Task").AddTypeArgumentListArguments(b.ReturnType)))
+              .NormalizeWhitespace();
+            */
         }
 
         private static Document Obfuscate(Document document)
         {
-            Document newDocument = document;
-            Console.WriteLine("Found document: " + document.Name + " [PRESS ENTER]");
+            Console.WriteLine("Found document: " + document.Name + "\n" + document.GetTextAsync().Result.ToString() + "\n[PRESS ENTER]");
             Console.ReadLine();
-            return newDocument;
+
+            var root = document.GetSyntaxRootAsync().Result;
+            //MyRewriter rewriter = new MyRewriter();
+            //rewriter.Visit(root);
+            var firstVariable = root.DescendantNodes().OfType<VariableDeclarationSyntax>().First();
+            var secureFloat = SyntaxFactory.VariableDeclaration(SyntaxFactory.IdentifierName("SecureFloat "), firstVariable.Variables);
+            var newRoot = root.ReplaceNode(firstVariable, secureFloat);
+            document = document.WithText(newRoot.GetText());
+
+            Console.WriteLine("New document:\n" + document.GetTextAsync().Result.ToString() + "\n[PRESS ENTER]");
+
+            return document;
         }
+    }
+
+    public class MyRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitVariableDeclaration(VariableDeclarationSyntax node)
+        {
+            Console.WriteLine("Found declaration: " + node.GetText() + ". Type " + node.Type + " var " + node.Variables.Count);
+            if (node.Variables.Count > 0)
+            {
+                if (node.Type.ToString() == SyntaxFactory.IdentifierName("float").ToString())
+                {
+                    var genericName = SyntaxFactory.GenericName(SyntaxFactory.Identifier("SecureFloat"));// = SecureFloat<>
+                    var newIdentifier = SyntaxFactory.IdentifierName("SecureFloat");
+                    var newType = node.Type.ReplaceNode(node.Type, newIdentifier);
+                    var newNode = SyntaxFactory.VariableDeclaration(newType, node.Variables);
+                    Console.WriteLine("t1: " + genericName.ToFullString() + " t2: " + newType.GetFirstToken() + " newNode: " + newNode.ToString() + " [PRESS ENTER]");
+                    Console.ReadLine();
+                    return newNode;
+                }
+            }
+            Console.WriteLine("[PRESS ENTER]");
+            Console.ReadLine();
+            //TypeSyntax secureFloatSyntax = new TypeSyntax();
+            return base.VisitVariableDeclaration(node);
+        }
+
+        /*
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax m)
+        {
+            var newReturnType = SF.GenericName(SF.Identifier("Task"), SF.TypeArgumentList(new SeparatedSyntaxList<TypeSyntax>().Add(m.ReturnType.WithoutTrivia())));
+            newReturnType = newReturnType.InsertTriviaBefore(newReturnType.GetLeadingTrivia().First(), m.ReturnType.GetLeadingTrivia().AsEnumerable());
+            newReturnType = newReturnType.InsertTriviaAfter(newReturnType.GetTrailingTrivia().First(), m.ReturnType.GetTrailingTrivia().AsEnumerable());
+            return m.ReplaceNode(m.ReturnType, newReturnType);
+        }
+        */
     }
 }
---
+++ SecureFloat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoObfuscation
+{
+    class SecureFloat
+    {
+        public float value1;
+        public float value2;
+        public float value3;
+
+        public SecureFloat(float f)
+        {
+            value1 = f + (f / 2);
+            value2 = f / 2;
+            value3 = f * 2;
+        }
+
+        public static implicit operator float(SecureFloat sf)
+        {
+            return 2 * (sf.value1 + sf.value2) - sf.value3;
+        }
+
+        public static implicit operator SecureFloat(float f)
+        {
+            return new SecureFloat(f);
+        }
+    }
+}
